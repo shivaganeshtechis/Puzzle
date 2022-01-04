ba9361b4e6d4a330c0b47d9a1038d46f3c17a966
shivaganeshtechis <shivaganesh@techis.io>
2022-01-04 13:20:12 +0530

    Finale

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import _ from "lodash";
+import "./App.css";
 
-class App extends Component {
+const layout = _.range(0, 16).map((n) => {
+  const row = Math.floor(n / 4);
+  const col = n % 4;
+  return [80 * col, 80 * row];
+});
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      positions: _.shuffle(_.range(0, 16)),
+    };
+  }
+  updatePosition(index) {
+    let { positions } = this.state;
+    let emptyIndex = positions.indexOf(0);
+    let targetIndex = positions.indexOf(index);
+    const dif = Math.abs(targetIndex - emptyIndex);
+    if (dif === 1 || dif === 4) {
+      positions[emptyIndex] = index;
+      positions[targetIndex] = 0;
+      this.setState({ positions });
+      let win = _.every(positions, (value, index, array) => {
+        value = value || 16;
+        return index === 0 || parseInt(array[index - 1]) <= parseInt(value);
+      });
+      if (win) {
+        window.alert("U Win!!!");
+      }
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="game">
+        {this.state.positions.map((i, key) => {
+          let cellClass = key ? "cell" : "empty cell";
+          let [x, y] = layout[this.state.positions.indexOf(key)];
+          return (
+            <div
+              key={key}
+              className={cellClass}
+              onClick={this.updatePosition.bind(this, key)}
+              style={{ transform: `translate3d(${x}px,${y}px,0) scale(1.1)` }}
+            >
+              {key}
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
-
 export default App;
